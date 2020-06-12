5b48c0fa6472cc42ee96ea1b290e8e798f073afd
Rocky <rrsalerno21@gmail.com>
2020-06-12 16:31:24 -0700

    added requires and middleware

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
