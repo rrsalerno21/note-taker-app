e62f52ba9200c3d292e0f2d47f7ae9d4cb84aa53
Rocky <rrsalerno21@gmail.com>
2020-06-12 17:22:46 -0700

    added get for /api/notes

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.get(`*`, function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', data => {
+        res.json(data);
+    });
+});
+
 app.listen(PORT, () => {
     console.log('App listen on PORT ' + PORT);
 });
