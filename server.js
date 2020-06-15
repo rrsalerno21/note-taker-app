597c0c7e43338e70e9afc099fdee72226eafe20b
Rocky <rrsalerno21@gmail.com>
2020-06-15 15:06:24 -0700

    added express static on server, fixed paths on notes.html

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get(`*`, function (req, res) {
+app.get(`/`, function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
@@ -22,6 +23,20 @@ app.get('/api/notes', (req, res) => {
         res.json(data);
     });
 });
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body;
+
+    fs.readFile('db.json', (err, data )=> {
+        if (err) throw err;
+        
+        const newNoteArray = data;
+
+        newNoteArray.push(note);
+
+        fs.writeFile('./db/db.json', newNoteArray);
+    });
+})
 
 app.listen(PORT, () => {
     console.log('App listen on PORT ' + PORT);
