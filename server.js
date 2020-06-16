544a9ee4dda050375518921108b241c0a6837463
Rocky <rrsalerno21@gmail.com>
2020-06-16 09:40:11 -0700

    added elete API and id key to /api/notes JSON

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.post('/api/notes', (req, res) => {
         let existArray = JSON.parse(data);
 
         console.log(existArray);
+
+        note.id = existArray.length + 1;
+        
         existArray.push(note);
         console.log('New array:')
         console.log(existArray);
@@ -50,6 +53,36 @@ app.post('/api/notes', (req, res) => {
     res.json(note);
     
 })
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    let noteID = parseInt(req.params.id);
+
+    console.log(noteID);
+
+    fs.readFile('db/db.json', function(err, data) {
+        if (err) throw err;
+        let existArray = JSON.parse(data);
+
+        const result = existArray.filter(ele => ele.id !== noteID)
+        
+        console.log('Filtered array:')
+        console.log(result);
+
+        result.forEach((ele, i) => {
+            ele.id = i + 1;
+        })
+        
+        console.log('New ID filter array:')
+        console.log(result);
+
+        fs.writeFile('db/db.json', JSON.stringify(result), err => {
+            if (err) throw err;
+            console.log('File writing success');
+        });
+
+        res.json(result)
+    })
+});
 
 app.listen(PORT, () => {
     console.log('App listen on PORT ' + PORT);
